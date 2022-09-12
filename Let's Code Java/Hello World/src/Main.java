caa9d680a4fd8ac6d003d191e52785542ac1aa2e
Caio Rodrigues <103125100+DevDorf@users.noreply.github.com>
2022-09-12 17:12:41 -0300

    Update Java

---
+++ Let's Code Java/Hello World/src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Olá Caio");
+
+        // Variáveis
+
+        String nome;
+        nome = "Camila";
+        nome = "Tuã";
+
+        // Operadores aritméticos
+
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtracao = a - b;
+        int multiplicacao = a * b;
+        float divisao = (float) a / b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+
+        // Operadores Lógicos
+
+        boolean resultado = false;
+        System.out.println(resultado);
+
+         boolean fimDeSemana = true;
+         
+
+
+    }
+}
